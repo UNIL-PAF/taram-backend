43c5798f39983f0a3c88c00a0f8cdc26d4e09e2d
Roman Mylonas <romanoman@gmail.com>
2022-06-21 11:53:40 +0200

    Create, save and load templates.

---
+++ src/main/kotlin/ch/unil/pafanalysis/controllers/TemplateController.kt
@@ -0,0 +1,38 @@
+package ch.unil.pafanalysis.controllers
+
+import ch.unil.pafanalysis.templates.model.Template
+import ch.unil.pafanalysis.templates.model.TemplateStep
+import ch.unil.pafanalysis.templates.service.TemplateRepository
+import ch.unil.pafanalysis.templates.service.TemplateService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+
+
+@CrossOrigin(origins = ["http://localhost:3000"], maxAge = 3600)
+@RestController
+// This means that this class is a Controller
+@RequestMapping(path = ["/template"])
+class TemplateController {
+
+    @Autowired
+    private var templateService: TemplateService? = null
+
+    @Autowired
+    private var templateRepository: TemplateRepository? = null
+
+    @PostMapping(path = ["/from-analysis/{analysisId}"])
+    fun createTemplate(@PathVariable(value = "analysisId") analysisId: Int, @RequestParam name: String? = null): Template? {
+        return templateService?.create(analysisId = analysisId, name = name)
+    }
+
+    @GetMapping()
+    fun getAllTemplates(): List<Template>? {
+        return templateRepository?.findAll()?.toList()
+    }
+
+    @GetMapping(path = ["/{templateId}"])
+    fun getTemplate(@PathVariable(value = "templateId") templateId: Int): Template? {
+        return templateRepository?.findById(templateId)
+    }
+
+}
---
+++ src/main/kotlin/ch/unil/pafanalysis/templates/service/TemplateService.kt
@@ -0,0 +1,66 @@
+package ch.unil.pafanalysis.templates.service
+
+import ch.unil.pafanalysis.analysis.model.AnalysisStep
+import ch.unil.pafanalysis.analysis.service.AnalysisRepository
+import ch.unil.pafanalysis.analysis.service.AnalysisService
+import ch.unil.pafanalysis.templates.model.Template
+import ch.unil.pafanalysis.templates.model.TemplateStep
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+
+@Service
+class TemplateService {
+
+    @Autowired
+    private var templateStepRepo: TemplateStepRepository? = null
+
+    @Autowired
+    private var templateRepo: TemplateRepository? = null
+
+    @Autowired
+    private var analysisRepo: AnalysisRepository? = null
+
+    @Autowired
+    private var analysisService: AnalysisService? = null
+
+
+    fun create(analysisId: Int, name: String?): Template? {
+        val analysis = analysisRepo?.findById(analysisId)
+        val origSteps: List<AnalysisStep>? = analysisService?.sortAnalysisSteps(analysis?.analysisSteps)
+
+        val templateName = (name?: analysis?.name)?: "template_" + analysis?.id
+        val template = templateRepo?.save(Template(id=0, name = templateName, lastModifDate = LocalDateTime.now()))
+
+        val templateSteps: List<TemplateStep?>? = origSteps?.map{ analysisToTemplateStep(it, template) }
+        val stepsWithNextIds = setNextIds(templateSteps!!)
+        stepsWithNextIds?.forEach{
+            templateStepRepo?.save(it!!)!!
+        }
+
+        return templateRepo?.findById(template!!.id!!)
+    }
+
+    private fun setNextIds(templateSteps: List<TemplateStep?>?): List<TemplateStep?>? {
+        val nrSteps: Int = templateSteps?.size?.minus(1)!!
+        return templateSteps?.mapIndexed{ i, step ->
+            if(i < nrSteps){
+                step!!.copy(nextId = templateSteps[i+1]!!.id)
+            }else{
+                step!!
+            }
+        }
+    }
+
+    private fun analysisToTemplateStep(analysisStep: AnalysisStep, template: Template?): TemplateStep {
+        return TemplateStep(
+            id = 0,
+            type = analysisStep.type,
+            lastModifDate = LocalDateTime.now(),
+            parameters = analysisStep.parameters,
+            parametersHash = analysisStep.parametersHash,
+            template = template
+        )
+    }
+
+}
