3a645ad795cf8765a6da9f522144591331122805
Roman Mylonas <romanoman@gmail.com>
2022-10-20 11:39:55 +0200

    Remove unused import.

--- src/main/kotlin/ch/unil/pafanalysis/PafAnalysisBackendApplication.kt
+++ src/main/kotlin/ch/unil/pafanalysis/PafAnalysisBackendApplication.kt
@@ -2,7 +2,6 @@ package ch.unil.pafanalysis
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
