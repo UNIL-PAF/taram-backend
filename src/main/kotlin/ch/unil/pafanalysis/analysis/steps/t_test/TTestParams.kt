79d2ee95f0882673dd80bb0f5caaefada41ba2d2
Roman Mylonas <romanoman@gmail.com>
2022-12-08 10:43:40 +0100

    Store first and second groups in parameters.

--- src/main/kotlin/ch/unil/pafanalysis/analysis/steps/t_test/TTestParams.kt
+++ src/main/kotlin/ch/unil/pafanalysis/analysis/steps/t_test/TTestParams.kt
@@ -3,7 +3,8 @@ package ch.unil.pafanalysis.analysis.steps.t_test
 data class TTestParams (
     val field: String? = null,
     val s0: Double? = null,
-    val firstGroup: String? = null,
+    val firstGroup: List<String>? = null,
+    val secondGroup: List<String>? = null,
     val twoSided: Boolean? = null,
     val multiTestCorr: String? = MulitTestCorr.BH.value,
     val signThres: Double? = 0.05,
