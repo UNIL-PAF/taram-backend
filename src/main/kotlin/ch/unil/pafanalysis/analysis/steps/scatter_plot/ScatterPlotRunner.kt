6477c920ba9f3ce2b80dd4d94c00c83c5cc82df1
Roman Mylonas <romanoman@gmail.com>
2023-06-08 14:54:50 +0200

    Add ScatterPlotPDF.

--- src/main/kotlin/ch/unil/pafanalysis/analysis/steps/scatter_plot/ScatterPlotRunner.kt
+++ src/main/kotlin/ch/unil/pafanalysis/analysis/steps/scatter_plot/ScatterPlotRunner.kt
@@ -5,16 +5,8 @@ import ch.unil.pafanalysis.analysis.model.AnalysisStepType
 import ch.unil.pafanalysis.analysis.steps.CommonRunner
 import ch.unil.pafanalysis.analysis.steps.CommonStep
 import ch.unil.pafanalysis.analysis.steps.EchartsPlot
-import ch.unil.pafanalysis.common.EchartsServer
-import com.itextpdf.kernel.geom.PageSize
 import com.itextpdf.kernel.pdf.PdfDocument
-import com.itextpdf.kernel.pdf.PdfReader
-import com.itextpdf.kernel.pdf.xobject.PdfFormXObject
-import com.itextpdf.layout.Document
 import com.itextpdf.layout.element.Div
-import com.itextpdf.layout.element.Image
-import com.itextpdf.layout.element.Paragraph
-import com.itextpdf.layout.element.Text
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
@@ -25,29 +17,17 @@ class ScatterPlotRunner() : CommonStep(), CommonRunner {
     override var type: AnalysisStepType? = AnalysisStepType.SCATTER_PLOT
 
     @Autowired
-    private var echartsServer: EchartsServer? = null
+    var asyncRunner: AsyncScatterPlotRunner? = null
 
     @Autowired
-    var asyncRunner: AsyncScatterPlotRunner? = null
+    var scatterPlotPdf: ScatterPlotPdf? = null
 
     fun getParameters(step: AnalysisStep?): ScatterPlotParams {
         return if(step?.parameters != null) gson.fromJson(step?.parameters, ScatterPlotParams().javaClass) else ScatterPlotParams()
     }
 
-    override fun createPdf(step: AnalysisStep, pdf: PdfDocument, plotWidth: Float, stepNr: Int): Div {
-        val title = Paragraph().add(Text(step.type).setBold())
-        val params = gson.fromJson(step.parameters, ScatterPlotParams::class.java)
-        val selCol = Paragraph().add(Text("Selected x-axis: ${params?.xAxis}"))
-
-        val div = Div()
-        div.add(title)
-        div.add(selCol)
-        val plot = echartsServer?.makeEchartsPlot(step, pdf, plotWidth)
-        div.add(plot)
-
-        if (step.comments !== null) div.add(Paragraph().add(Text(step.comments)))
-
-        return div
+    override fun createPdf(step: AnalysisStep, pdf: PdfDocument, plotWidth: Float, stepNr: Int): Div? {
+       return scatterPlotPdf?.createPdf(step, pdf, plotWidth, stepNr)
     }
 
     override fun run(oldStepId: Int, step: AnalysisStep?, params: String?): AnalysisStep {
