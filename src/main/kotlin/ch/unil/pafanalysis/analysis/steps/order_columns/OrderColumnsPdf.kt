647baab22afc74b75bb89feee10cc235349c9a87
Roman Mylonas <romanoman@gmail.com>
2023-07-14 13:28:37 +0200

    Adapt order columns pdf.

--- src/main/kotlin/ch/unil/pafanalysis/analysis/steps/order_columns/OrderColumnsPdf.kt
+++ src/main/kotlin/ch/unil/pafanalysis/analysis/steps/order_columns/OrderColumnsPdf.kt
@@ -1,10 +1,12 @@
 package ch.unil.pafanalysis.analysis.steps.order_columns
 
 import ch.unil.pafanalysis.analysis.model.AnalysisStep
+import ch.unil.pafanalysis.analysis.steps.t_test.TTestParams
 import ch.unil.pafanalysis.pdf.PdfCommon
 import com.itextpdf.kernel.pdf.PdfDocument
 import com.itextpdf.layout.element.Div
 import com.itextpdf.layout.element.Paragraph
+import com.itextpdf.layout.element.Table
 import org.springframework.stereotype.Service
 
 
@@ -13,15 +15,30 @@ class OrderColumnsPdf() : PdfCommon() {
 
     fun createPdf(step: AnalysisStep, pdf: PdfDocument?, plotWidth: Float, stepNr: Int): Div? {
         val res = gson.fromJson(step.results, OrderColumns::class.java)
+        val parsedParams = gson.fromJson(step.parameters, OrderColumnsParams::class.java)
 
         val stepDiv = Div()
         stepDiv.add(titleDiv("$stepNr - Order columns", plotWidth = plotWidth))
 
-        val tableData: List<Pair<String, Paragraph?>> = listOf(
-            "Nr of columns moved" to Paragraph(res.nrColumnsMoved.toString())
-        )
+        val colTable = Table(3)
+        colTable.setWidth(plotWidth)
+        val cellFifth = plotWidth/5
 
-        stepDiv.add(addTwoRowTable(tableData))
+        // 1. parameters
+        val paramsDiv = Div()
+        if(parsedParams.moveSelIntFirst == true) paramsDiv.add(getParagraph("Move default intensity [${step.columnInfo?.columnMapping?.intCol}] first."))
+        colTable.addCell(getParamsCell(paramsDiv, 2*cellFifth))
+
+        // 2. data
+        colTable.addCell(getDataCell(Div(), 2 * cellFifth))
+
+        // 3. results
+        val rightDiv = Div()
+        rightDiv.add(getParagraph("${step.nrProteinGroups} protein groups"))
+        rightDiv.add(getParagraph("Table ${step.tableNr}"))
+        colTable.addCell(getResultCell(rightDiv, cellFifth))
+
+        stepDiv.add(colTable)
         return stepDiv
     }
 
