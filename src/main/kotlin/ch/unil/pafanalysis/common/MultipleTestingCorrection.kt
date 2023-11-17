84a0d6e51f65febddbd7b2f7d27fe7951a3b66fa
Roman Mylonas <romanoman@gmail.com>
2023-11-17 14:29:20 +0100

    FDR correction of unordered p-values.

--- src/main/kotlin/ch/unil/pafanalysis/common/MultipleTestingCorrection.kt
+++ src/main/kotlin/ch/unil/pafanalysis/common/MultipleTestingCorrection.kt
@@ -4,39 +4,27 @@ class MultipleTestingCorrection {
 
     fun fdrCorrection(pVals: List<Double>): List<Double> {
         val m = pVals.size
-        var adjustedPvalues: Array<Double?> = arrayOfNulls<Double>(m)
+        var adjustedPvalues: Array<Double?> = arrayOfNulls(m)
+        val pValsWithIdx = pVals.zip(pVals.indices)
 
-            // order the pvalues.
-            val pvalues = pVals.sorted()
+        // order the pvalues.
+        val (pvalues, idx) = pValsWithIdx.sortedBy { it.first }.unzip() // pVals.sorted()
 
-            // iterate through all p-values:  largest to smallest
-            for(i in m-1 downTo 0){
-                if(i == m-1){
-                    adjustedPvalues[i] = pvalues[i]
-                } else {
-                    val unadjustedPvalue = pvalues[i];
-                    val divideByM: Double = (i + 1).toDouble();
-                    val left: Double = adjustedPvalues[i + 1] ?: throw Exception("adjustedPvalues out of bound")
-                    val right: Double = (m / divideByM) * unadjustedPvalue;
-                    adjustedPvalues[i] = left.coerceAtMost(right);
-                }
-        /*
-
-            for ( i: Int = m - 1; i >= 0; i--) {
+        // iterate through all p-values:  largest to smallest
+        for (i in m - 1 downTo 0) {
             if (i == m - 1) {
-                adjustedPvalues[i] = pvalues[i];
+                adjustedPvalues[i] = pvalues[i]
             } else {
-                double unadjustedPvalue = pvalues[i];
-                int divideByM = i + 1;
-                double left = adjustedPvalues[i + 1];
-                double right = (m / (double) divideByM) * unadjustedPvalue;
-                adjustedPvalues[i] = Math.min(left, right);
+                val unadjustedPvalue = pvalues[i];
+                val divideByM: Double = (i + 1).toDouble();
+                val left: Double = adjustedPvalues[i + 1] ?: throw Exception("AdjustedPValues out of bound")
+                val right: Double = (m / divideByM) * unadjustedPvalue;
+                adjustedPvalues[i] = left.coerceAtMost(right);
             }
         }
 
-         */
-        }
-        return adjustedPvalues.map{ it ?: throw Exception("AdjustedPValue cannot be null.") }.toList()
+        val adjustedZipped = adjustedPvalues.map { it ?: throw Exception("AdjustedPValue cannot be null.") }.toList().zip(idx)
+        return adjustedZipped.sortedBy { it.second }.map{it.first}
     }
 
 }
