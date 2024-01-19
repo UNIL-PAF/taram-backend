c330f25fec31c977b88b2f95b791cfb546517336
Roman Mylonas <romanoman@gmail.com>
2024-01-19 08:57:54 +0100

    Adapt headers to log2.

--- src/main/kotlin/ch/unil/pafanalysis/analysis/steps/log_transformation/AsyncLogTransformationRunner.kt
+++ src/main/kotlin/ch/unil/pafanalysis/analysis/steps/log_transformation/AsyncLogTransformationRunner.kt
@@ -1,6 +1,7 @@
 package ch.unil.pafanalysis.analysis.steps.log_transformation
 
 import ch.unil.pafanalysis.analysis.model.AnalysisStep
+import ch.unil.pafanalysis.analysis.model.Header
 import ch.unil.pafanalysis.analysis.steps.CommonStep
 import ch.unil.pafanalysis.analysis.steps.summary_stat.SummaryStat
 import ch.unil.pafanalysis.common.ReadTableData
@@ -24,54 +25,60 @@ class AsyncLogTransformationRunner() : CommonStep() {
     fun runAsync(oldStepId: Int, newStep: AnalysisStep?) {
         val funToRun: () -> AnalysisStep? = {
             val params = gson.fromJson(newStep?.parameters, LogTransformationParams().javaClass)
-
-            val logTransRes = transformTable(
-                newStep,
-                params
-            )
+            val table = readTableData.getTable(getOutputRoot() + newStep?.resultTablePath, newStep?.commonResult?.headers)
+            val (selHeaders, ints) = getSelTable(table, newStep, params)
+            val transInts = logComp!!.runTransformation(ints, params)
+            val newCols = computeTransTable(transInts, table, selHeaders, params)
+            val newHeaders = adaptColNames(selHeaders, table.headers, params)
+            writeTableData.write(getOutputRoot() + newStep?.resultTablePath, Table(newHeaders, newCols))
+            val logTransRes = getTransformation(selHeaders, transInts)
 
             val commonResult =
-                newStep?.commonResult?.copy(intColIsLog = params.transformationType == TransformationType.LOG2.value)
+                newStep?.commonResult?.copy(
+                    intColIsLog = params.transformationType == TransformationType.LOG2.value,
+                    headers = newHeaders
+                )
 
             newStep?.copy(
                 results = gson.toJson(logTransRes),
                 commonResult = commonResult,
-
                 )
         }
-
         tryToRun(funToRun, newStep)
     }
 
-    private fun getSelHeaders(): Table {
-        val intCol = params.intCol ?: step?.columnInfo?.columnMapping?.intCol
-        val table = readTableData.getTable(getOutputRoot() + step?.resultTablePath, step?.commonResult?.headers)
-    }
-
-    private fun transformTable(
-        step: AnalysisStep?,
-        params: LogTransformationParams
-    ): LogTransformation? {
-        val intCol = params.intCol ?: step?.columnInfo?.columnMapping?.intCol
-        val table = readTableData.getTable(getOutputRoot() + step?.resultTablePath, step?.commonResult?.headers)
-
+    private fun getSelTable(table: Table?, step: AnalysisStep?, params: LogTransformationParams): Pair<List<Header>, List<List<Double>>> {
         val (selHeaders, ints) = if(params.selColIdxs != null){
             val selCols = table?.headers?.filter{params.selColIdxs.contains(it.idx)} ?: emptyList()
             Pair(selCols, readTableData.getDoubleMatrix(table, selCols))
         }else{
+            val intCol = params.intCol ?: step?.columnInfo?.columnMapping?.intCol
             readTableData.getDoubleMatrix(table, intCol, step?.columnInfo?.columnMapping?.experimentDetails)
         }
+        return Pair(selHeaders, ints)
+    }
 
-        val transInts = logComp!!.runTransformation(ints, params)
-        val newCols: List<List<Any>>? = table.cols?.mapIndexed { i, c ->
+    private fun adaptColNames(selHeaders: List<Header>, tableHeaders: List<Header>?, params: LogTransformationParams): List<Header>? {
+        return if(params.adaptHeaders == true){
+            val selHeaderIdxs = selHeaders.map{it.idx}
+            tableHeaders?.map{ h ->
+                if(selHeaderIdxs.contains(h.idx)){
+                    h.copy(name = h.name + "." + params.transformationType)
+                }else h
+            }
+        } else tableHeaders
+    }
+
+    private fun computeTransTable(transInts: List<List<Double>>, table: Table?, selHeaders: List<Header>, params: LogTransformationParams): List<List<Any>>?{
+        return table?.cols?.mapIndexed { i, c ->
             val selHeader = selHeaders.withIndex().find { it.value.idx == i }
             if (selHeader != null) {
                 transInts[selHeader.index]
             } else c
         }
+    }
 
-        writeTableData.write(getOutputRoot() + step?.resultTablePath, table.copy(cols = newCols))
-
+    private fun getTransformation(selHeaders: List<Header>, transInts: List<List<Double>>): LogTransformation? {
         val summaryStatComp = SummaryStatComputation()
         val stat = summaryStatComp.getBasicSummaryStat(transInts, selHeaders)
 
--- src/main/kotlin/ch/unil/pafanalysis/analysis/steps/log_transformation/LogTransformationParams.kt
+++ src/main/kotlin/ch/unil/pafanalysis/analysis/steps/log_transformation/LogTransformationParams.kt
@@ -3,7 +3,8 @@ package ch.unil.pafanalysis.analysis.steps.log_transformation
 data class LogTransformationParams (
     val transformationType: String? = null,
     val intCol: String? = null,
-    val selColIdxs: List<Int>? = null
+    val selColIdxs: List<Int>? = null,
+    val adaptHeaders: Boolean? = null
 )
 
 enum class TransformationType(val value: String) {
