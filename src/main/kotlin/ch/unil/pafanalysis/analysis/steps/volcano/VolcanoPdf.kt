a198296982289320f2e9f95f625ccaa44f4b7e9d
Roman Mylonas <romanoman@gmail.com>
2024-11-15 15:27:33 +0100

    PDF: show volcano plot description only on first plot.

--- src/main/kotlin/ch/unil/pafanalysis/analysis/steps/volcano/VolcanoPdf.kt
+++ src/main/kotlin/ch/unil/pafanalysis/analysis/steps/volcano/VolcanoPdf.kt
@@ -1,6 +1,8 @@
 package ch.unil.pafanalysis.analysis.steps.volcano
 
 import ch.unil.pafanalysis.analysis.model.AnalysisStep
+import ch.unil.pafanalysis.analysis.model.AnalysisStepType
+import ch.unil.pafanalysis.analysis.service.AnalysisStepRepository
 import ch.unil.pafanalysis.analysis.steps.scatter_plot.ScatterPlot
 import ch.unil.pafanalysis.analysis.steps.scatter_plot.ScatterPlotParams
 import ch.unil.pafanalysis.common.EchartsServer
@@ -18,9 +20,16 @@ class VolcanoPdf() : PdfCommon() {
     @Autowired
     private var echartsServer: EchartsServer? = null
 
+    @Autowired
+    private var analysisStepRepo: AnalysisStepRepository? = null
+
     fun createPdf(step: AnalysisStep, pdf: PdfDocument, plotWidth: Float, stepNr: Int): Div {
         val div = Div()
-        val description = "y-axis: can be either adjusted or unadjusted p-values (-log10)."
+
+        // check if there is already a volcano plot shown before
+        val volcanoBefore = findVolcanoBefore(step?.beforeId)
+        val description = if(volcanoBefore == false) "y-axis: can be either adjusted or unadjusted p-values (-log10)." else null
+
         div.add(titleDiv("$stepNr. Volcano plot", plotWidth, description = description, link = "$stepNr-${step.type}"))
 
         div.add(Paragraph(" "))
@@ -35,6 +44,12 @@ class VolcanoPdf() : PdfCommon() {
         if(hasMultiGene == true) div.add(getParagraph("* only the first of multiple gene names is displayed."))
 
         return div
+    }
+
+    private fun findVolcanoBefore(stepId: Int?): Boolean? {
+        if(stepId == null) return false
+        val step = analysisStepRepo?.findById(stepId)
+        return if(step?.type == AnalysisStepType.VOLCANO_PLOT.value) true else findVolcanoBefore(step?.beforeId)
     }
 
 }
