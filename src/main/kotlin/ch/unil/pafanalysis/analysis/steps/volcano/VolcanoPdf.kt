5b10212abd40de2b7a3d2fbf6c3d0a0387ff8e04
Roman Mylonas <romanoman@gmail.com>
2024-11-16 08:27:26 +0100

    Volcano plot pdf: change text.

--- src/main/kotlin/ch/unil/pafanalysis/analysis/steps/volcano/VolcanoPdf.kt
+++ src/main/kotlin/ch/unil/pafanalysis/analysis/steps/volcano/VolcanoPdf.kt
@@ -28,7 +28,8 @@ class VolcanoPdf() : PdfCommon() {
 
         // check if there is already a volcano plot shown before
         val volcanoBefore = findVolcanoBefore(step?.beforeId)
-        val description = if(volcanoBefore == false) "y-axis: can be either adjusted or unadjusted p-values (-log10)." else null
+        val description =
+            if (volcanoBefore == false) "Common way to represent statistical significance (y-axis) and fold change (x-axis). By using -log10 transformation, low p-values are displayed at the top of the plot." else null
 
         div.add(titleDiv("$stepNr. Volcano plot", plotWidth, description = description, link = "$stepNr-${step.type}"))
 
@@ -38,18 +39,18 @@ class VolcanoPdf() : PdfCommon() {
 
         val volcanoParams = gson.fromJson(step?.parameters, VolcanoPlotParams().javaClass)
         val volcanoPlot = gson.fromJson(step?.results, VolcanoPlot().javaClass)
-        val hasMultiGene = volcanoPlot.data?.any{ a ->
+        val hasMultiGene = volcanoPlot.data?.any { a ->
             volcanoParams.selProteins?.contains(a.prot) ?: false && a.multiGenes ?: false
         }
-        if(hasMultiGene == true) div.add(getParagraph("* only the first of multiple gene names is displayed."))
+        if (hasMultiGene == true) div.add(getParagraph("* only the first of multiple gene names is displayed."))
 
         return div
     }
 
     private fun findVolcanoBefore(stepId: Int?): Boolean? {
-        if(stepId == null) return false
+        if (stepId == null) return false
         val step = analysisStepRepo?.findById(stepId)
-        return if(step?.type == AnalysisStepType.VOLCANO_PLOT.value) true else findVolcanoBefore(step?.beforeId)
+        return if (step?.type == AnalysisStepType.VOLCANO_PLOT.value) true else findVolcanoBefore(step?.beforeId)
     }
 
 }
