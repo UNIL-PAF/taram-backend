caaf1a394f78c810df3d2f31c841ad6f46457ec8
Roman Mylonas <romanoman@gmail.com>
2025-01-31 15:13:40 +0100

    HtmlScatterPlot: replace function containing fix variables.

--- src/main/kotlin/ch/unil/pafanalysis/html_plot/HtmlScatterPlot.kt
+++ src/main/kotlin/ch/unil/pafanalysis/html_plot/HtmlScatterPlot.kt
@@ -38,8 +38,56 @@ object HtmlScatterPlot {
           var otherField = "$otherFieldValue";  
         """.trimIndent()
 
-
-        return html?.replace("__SCRIPT__", colorBy + otherField + script)
+        return replaceTooltip(html?.replace("__SCRIPT__", colorBy + otherField + script))
     }
+
+    private fun replaceTooltip(html: String?): String? {
+
+        val originalFunction = """
+            function (p) {
+                const text1 = "<strong>" + p.data[2] + "</strong><br>"
+                const text2 = p.data[3] + "<br>"
+                const text3 = params.xAxis + ": <strong>" + String(p.data[0].length > 5 ? p.data[0].toExponential(1) : p.data[0].toFixed(1)) + "</strong><br>"
+                const text4 = params.yAxis + ": <strong>" + String(p.data[1].length > 5 ? p.data[1].toExponential(1) : p.data[1].toFixed(1)) + "</strong><br>"
+                const text5 = (params.colorBy) ? (params.colorBy + ": <strong>" + p.data[4].toFixed(1) + "</strong><br>") : ""
+                const text6 = otherField ? (otherField + ": <strong>" + p.data[6] + "</strong><br>") : ""
+                return text1 + text2 + text3 + text4 + text5 + text6
+            }
+        """.trimIndent()
+
+        val funFound = extractFunction(html)
+
+        return if(funFound != null) html?.replace(funFound, originalFunction) else html
+    }
+
+    private fun extractFunction(text: String?): String? {
+        if (text.isNullOrEmpty()) return null
+
+        val selText = Regex("""tooltip(.+)series""").find(text)?.groups?.get(1)?.value ?: return null
+
+        //val textAfterTooltip = text.replace(Regex(""".+tooltip"""), "")
+
+        val pattern = Regex("""function\s*\([^)]*\)\s*\{""")  // Matches function(x){
+        val match = pattern.find(selText) ?: return null  // Find function declaration
+
+        val startIndex = match.range.first
+
+        var braceCount: Int? = null
+        var endIndex = startIndex
+
+        for (i in startIndex until selText.length) {
+            when (selText[i]) {
+                '{' -> if(braceCount == null) braceCount = 1 else braceCount++
+                '}' -> if(braceCount != null) braceCount--
+            }
+            if (braceCount == 0) {  // Found matching closing brace
+                endIndex = i + 1
+                break
+            }
+        }
+
+        return selText.substring(startIndex, endIndex)
+    }
+
 
 }
