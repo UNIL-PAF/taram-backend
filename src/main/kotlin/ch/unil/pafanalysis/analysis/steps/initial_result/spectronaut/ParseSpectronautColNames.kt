de9849168850f1cf23a48d9fa77c673a9323aeb9
Roman Mylonas <romanoman@gmail.com>
2026-01-06 12:23:54 +0100

    Fix bug: ColumnMappingParserTests not passing.

--- src/main/kotlin/ch/unil/pafanalysis/analysis/steps/initial_result/spectronaut/ParseSpectronautColNames.kt
+++ src/main/kotlin/ch/unil/pafanalysis/analysis/steps/initial_result/spectronaut/ParseSpectronautColNames.kt
@@ -4,15 +4,20 @@ object ParseSpectronautColNames {
 
     fun getCommonStartAndEnd(colNames: List<String>, selField: String? = null): Pair<String, String> {
         // remove the first part until the first_
-        val cleanSelCols = colNames.map{it.replace(Regex("^.+?_(?=([A-Za-z0-9]))"), "")}
+        val cleanSelCols = colNames.map{it.replace(Regex("^.+?_(?=([A-Za-z]))"), "")}
 
-        val commonStart = cleanSelCols.fold(cleanSelCols.first()){ a, v ->
+        // in case nothing got removed, we have to remove up to the first number
+        val cleanSelCols2 = if(cleanSelCols.first() == colNames.first()) {
+            colNames.map { it.replace(Regex("^.+?_(?=([A-Za-z0-9]))"), "") }
+        } else cleanSelCols
+
+        val commonStart = cleanSelCols2.fold(cleanSelCols2.first()){ a, v ->
             v.commonPrefixWith(a)
         }.replace(Regex("^(\\d+)$"), "")
             .replace(Regex("_$"), "")
             .replace(Regex("_(\\d+)$"), "")
 
-        val commonEnd = cleanSelCols.fold(cleanSelCols.first()){a, v -> v.commonSuffixWith(a)}
+        val commonEnd = cleanSelCols2.fold(cleanSelCols2.first()){a, v -> v.commonSuffixWith(a)}
         val commonEnd2 = if(selField != null) commonEnd.replace(selField, "") else commonEnd
 
         return Pair(commonStart, commonEnd2)
