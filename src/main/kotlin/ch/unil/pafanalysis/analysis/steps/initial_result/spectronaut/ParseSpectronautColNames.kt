a7e02c2db8c0f4ddb38dd843cafa6bc618a7473d
Roman Mylonas <romanoman@gmail.com>
2026-01-06 11:49:58 +0100

    Fix bug: cannot import Richetin_20605-42.

--- src/main/kotlin/ch/unil/pafanalysis/analysis/steps/initial_result/spectronaut/ParseSpectronautColNames.kt
+++ src/main/kotlin/ch/unil/pafanalysis/analysis/steps/initial_result/spectronaut/ParseSpectronautColNames.kt
@@ -4,11 +4,13 @@ object ParseSpectronautColNames {
 
     fun getCommonStartAndEnd(colNames: List<String>, selField: String? = null): Pair<String, String> {
         // remove the first part until the first_
-        val cleanSelCols = colNames.map{it.replace(Regex("^.+?_(?=([A-Z|a-z]))"), "")}
+        val cleanSelCols = colNames.map{it.replace(Regex("^.+?_(?=([A-Za-z0-9]))"), "")}
 
-        val commonStart = cleanSelCols.fold(cleanSelCols.first()){ a, v -> v.commonPrefixWith(a)}
-            .replace(Regex("_(\\d+)$"), "")
+        val commonStart = cleanSelCols.fold(cleanSelCols.first()){ a, v ->
+            v.commonPrefixWith(a)
+        }.replace(Regex("^(\\d+)$"), "")
             .replace(Regex("_$"), "")
+            .replace(Regex("_(\\d+)$"), "")
 
         val commonEnd = cleanSelCols.fold(cleanSelCols.first()){a, v -> v.commonSuffixWith(a)}
         val commonEnd2 = if(selField != null) commonEnd.replace(selField, "") else commonEnd
