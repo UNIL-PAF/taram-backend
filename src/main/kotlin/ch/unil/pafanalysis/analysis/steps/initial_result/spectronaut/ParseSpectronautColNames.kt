867edd5d9bc938d324fe5525c5cc6d1ae9925a51
Roman Mylonas <romanoman@gmail.com>
2026-01-06 14:10:46 +0100

    Fix test: InitialResultRunnerTests

--- src/main/kotlin/ch/unil/pafanalysis/analysis/steps/initial_result/spectronaut/ParseSpectronautColNames.kt
+++ src/main/kotlin/ch/unil/pafanalysis/analysis/steps/initial_result/spectronaut/ParseSpectronautColNames.kt
@@ -13,14 +13,14 @@ object ParseSpectronautColNames {
 
         val commonStart = cleanSelCols2.fold(cleanSelCols2.first()){ a, v ->
             v.commonPrefixWith(a)
-        }.replace(Regex("^(\\d+)$"), "")
-            .replace(Regex("_$"), "")
-            .replace(Regex("_(\\d+)$"), "")
+        }.replace(Regex("_(\\d+)$"), "").replace(Regex("_$"), "")
+
+        val commonStart2 = if(commonStart.toIntOrNull() != null) "" else commonStart
 
         val commonEnd = cleanSelCols2.fold(cleanSelCols2.first()){a, v -> v.commonSuffixWith(a)}
         val commonEnd2 = if(selField != null) commonEnd.replace(selField, "") else commonEnd
 
-        return Pair(commonStart, commonEnd2)
+        return Pair(commonStart2, commonEnd2)
     }
 
 }
